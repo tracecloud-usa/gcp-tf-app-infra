98ab17826e8dedd7815e2f487c5cc0717aa44854
ahmutysj3 <ahmutysj3@gmail.com>
2024-08-22 22:20:36 -0400

    comment out instance

--- main.tf
+++ main.tf
@@ -23,24 +23,6 @@ resource "google_compute_instance" "this" {
   }
 }
 
-variable "instances" {
-  type = list(object({
-    name            = string
-    app_project     = string
-    service_account = string
-    network         = string
-    network_project = string
-    subnet          = string
-  }))
-  default = [{
-    name            = "test-web-server"
-    app_project     = "product-app-prod-01"
-    service_account = "nginx-web-admin"
-    network         = "vpc-edge-untrusted"
-    network_project = "vpc-edge-prod-01"
-    subnet          = "edge-untrusted-subnet-01"
-  }]
-}
 
 data "google_compute_subnetwork" "this" {
   for_each = { for k, v in var.instances : v.subnet => v }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,25 @@ variable "provider_config" {
     region  = string
   })
 }
+
+variable "instances" {
+  type = list(object({
+    name            = string
+    app_project     = string
+    service_account = string
+    network         = string
+    network_project = string
+    subnet          = string
+  }))
+  default = []
+  /*   default = [
+    {
+      name            = "test-web-server"
+      app_project     = "product-app-prod-01"
+      service_account = "nginx-web-admin"
+      network         = "vpc-edge-untrusted"
+      network_project = "vpc-edge-prod-01"
+      subnet          = "edge-untrusted-subnet-01"
+    },
+  ] */
+}
