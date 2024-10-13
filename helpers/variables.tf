7fce521f69b01ad27f82ed786be775de1e55173e
ahmutysj3 <ahmutysj3@gmail.com>
2024-10-13 15:53:35 -0400

    added test authentication button and success page

--- helpers/variables.tf
+++ helpers/variables.tf
@@ -8,25 +8,3 @@ variable "config" {
     region  = "us-east4"
   }
 }
-
-variable "websites" {
-  type = list(object({
-    domain     = string
-    files      = list(string)
-    host       = string
-    web_server = string
-  }))
-  default = [{
-    host       = "nginx"
-    domain     = "test.tracecloud.us"
-    web_server = "web-node-01"
-    files = [
-      "index.html",
-      "styles.css",
-      "banner-background.jpg",
-      "contact.html",
-      "pricing.html",
-      "features.html",
-    ]
-  }]
-}
